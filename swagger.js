1cc3c209e0baa76c353a2c0f07570421b829f867
betpractice116 <bestpractice8116@gmail.com>
2025-01-20 05:11:25 -0800

    fix: swagger ui

--- swagger.js
+++ swagger.js
@@ -1,22 +1,28 @@
-const swaggerJsDoc = require("swagger-jsdoc");
-var SwaggerUIBundle = require('swagger-ui-dist').SwaggerUIBundle
+const swaggerJsDoc = require('swagger-jsdoc');
+var SwaggerUIBundle = require('swagger-ui-dist').SwaggerUIBundle;
 
 const options = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "Express API Documentation",
-      version: "1.0.0",
-      description: "API documentation for my Express.js backend",
+      title: 'Express API Documentation',
+      version: '1.0.0',
+      description: 'API documentation for my Express.js backend'
     },
     servers: [
       {
-        url: "http://localhost:5173", // Replace with your server URL
-        layout: "StandaloneLayout"
-      },
-    ],
+        url: 'https://simple-crud-ldzp.onrender.com:5173', // Replace with your server URL
+        layout: 'StandaloneLayout'
+      }
+    ]
   },
-  apis: ["./index.js", "./src/router/*.js", "./src/controller/*.js", "./src/services/*.js", "./src/middleware/*.js"],
+  apis: [
+    './index.js',
+    './src/router/*.js',
+    './src/controller/*.js',
+    './src/services/*.js',
+    './src/middleware/*.js'
+  ]
 };
 
 const swaggerSpec = swaggerJsDoc(options);
